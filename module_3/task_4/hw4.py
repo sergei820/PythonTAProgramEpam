68f9a1736181d7d0d7f7026d4ecc15b34ba1d24d
Sergei_Kamendrovskii <se25051990@gmail.com>
2023-09-15 19:25:04 +0400

    Module 3 Task 1 - Done Task 2 - Done Task 3 - Done Task 4 - Passes tests

--- module_3/task_4/hw4.py
+++ module_3/task_4/hw4.py
@@ -12,13 +12,28 @@ assert combinations([1, 2], [3, 4]) == [
 ]
 """
 from typing import Any, List
+from itertools import product
 
 
 def combinations(*args: List[Any]) -> List[List]:
-    result = []
-    number_of_lists = len(args)
-    for i in range(len(args)):
-        for el in args[i]:
-            pass
+     # result = []
+    # temp_list = []
 
+    # def generate_combinations(list_index):
+    #     if list_index == len(args):
+    #         result.append(temp_list.copy())
+    #         return result
+    #     for el in args[list_index]:
+    #         temp_list.append(el)
+    #         generate_combinations(list_index + 1)
+    #         temp_list.pop()
+    #
+    # generate_combinations(0)
+    # return result
+    result = list(product(*args))
+    return [list(combination) for combination in result]
+
+
+for results in combinations([[1, 2, 3], [4, 5, 6], [7, 8, 9]]):
+    print(results)
 
