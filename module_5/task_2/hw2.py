373573799340584997cb477a72c9f1421d529264
Sergei_Kamendrovskii <se25051990@gmail.com>
2023-09-24 19:01:51 +0400

    module 5 task 2 - in progress

--- module_5/task_2/hw2.py
+++ module_5/task_2/hw2.py
@@ -1,7 +1,10 @@
 import sqlite3
+from collections import namedtuple
 
 
 class TableData:
+    BookRecord = namedtuple('title', 'author')
+
     def __init__(self, database_name: str, table_name: str = None):
         self.database_name = database_name
         if table_name:
@@ -24,18 +27,69 @@ class TableData:
             table_names = [table[0] for table in table_names]
             return table_names
 
-    def get_table_data(self, table_name: str = None):
-        if not table_name:
-            table_name = self.table_name
+    def get_table_data(self):
         with sqlite3.connect(self.database_name) as connection:
             cursor = connection.cursor()
-            cursor.execute(f'SELECT * from {table_name}')
+            cursor.execute(f'SELECT * from {self.table_name}')
             data = cursor.fetchall()
             return data
 
+    def __len__(self):
+        with sqlite3.connect(self.database_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute(f'SELECT count(*) from {self.table_name}')
+            return cursor.fetchone()[0]
+
+    def __getitem__(self, item):
+        for row in self.get_table_data():
+            if row[1] == item:
+                return row
+
+    def __contains__(self, item):
+        for row in self.get_table_data():
+            if row[1] == item:
+                return True
+        return False
+
+    def __iter__(self):
+        with sqlite3.connect(self.database_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute(f'SELECT * from {self.table_name}')
+            while True:
+                row_in_table = cursor.fetchone()
+                if row_in_table is None:
+                    break
+                yield row_in_table
+
 
 if __name__ == "__main__":
-    table_data = TableData('example.sqlite', 'books')
-    rows = table_data.get_table_data()
+    books_table = TableData('example.sqlite', 'books')
+    rows = books_table.get_table_data()
     for row in rows:
         print(row)
+    print()
+    print(len(books_table))
+    print(books_table['Bradbury'])
+    print('Yeltsin' in books_table)
+    print('Bradbury' in books_table)
+    for book in books_table:
+        print(book['author'])
+
+
+# Write a wrapper class TableData for database table, that when initialized with database name and table
+# acts as collection object (implements Collection protocol).
+# Assume all data has unique values in 'author' column.
+# So, if books = TableData(database_name='example.sqlite', table_name='books')
+#
+# then
+#  -  `len(books)` will give current amount of rows in books table in database
+#  -  `books['Bradbury']` should return single data row for book with author Bradbury
+#  -  `'Yeltsin' in books` should return if book with same author exists in table
+#  -  object implements iteration protocol. i.e. you could use it in for loops::
+#        for book in books:
+#            print(book['author'])
+#  - all above mentioned calls should reflect most recent data.
+#  If data in table changed after you created collection instance, your calls should return updated data.
+#
+# Avoid reading entire table into memory. When iterating through records, start reading the first record, then go to the next one, until records are exhausted.
+# When writing tests, it's not always necessary to mock database calls completely. Use supplied example.sqlite file as database fixture file.
