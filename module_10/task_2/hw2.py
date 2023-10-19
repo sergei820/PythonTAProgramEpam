196bce3ce6d2ed5454de20e900b9b71a9277fd28
Sergei_Kamendrovskii <se25051990@gmail.com>
2023-10-19 19:37:31 +0400

    module 10 task 1 - done task 2 - done

--- module_10/task_2/hw2.py
+++ module_10/task_2/hw2.py
@@ -28,3 +28,40 @@ assert order_1.final_price() == 50
 order_2 = Order(100, elder_discount)
 assert order_1.final_price() == 10
 """
+
+
+class Order:
+    def __init__(self, price, discount_strategy=None):
+        self.price = price
+        self.discount_strategy = discount_strategy
+
+    def final_price(self):
+        if self.discount_strategy:
+            return int(self.discount_strategy.apply_discount(self.price))
+        return self.price
+
+
+class DiscountStrategy:
+    def apply_discount(self, price):
+        pass
+
+
+class MorningDiscount(DiscountStrategy):
+    def apply_discount(self, price):
+        return price / 2
+
+
+class ElderDiscount(DiscountStrategy):
+    def apply_discount(self, price):
+        return price / 10
+
+
+morning_discount = MorningDiscount()
+elder_discount = ElderDiscount()
+
+
+order_1 = Order(100, morning_discount)
+assert order_1.final_price() == 50
+
+order_2 = Order(100, elder_discount)
+assert order_2.final_price() == 10
